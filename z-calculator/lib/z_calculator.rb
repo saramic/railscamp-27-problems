78d48e5ffd8770e8ffecd6438a159efa5f1dab6e
Michael Milewski <saramic@gmail.com>
2022-02-06 11:01:44 +1030

    implemented indeterminate number adding
    Co-authored-by: luca Cave <lucacave2@gmail.com>
    Co-authored-by: Emile Violi <efvioli@gmail.com>
    Co-authored-by: Ben Hull <ben@companionstudio.com.au>

--- z-calculator/lib/z_calculator.rb
+++ z-calculator/lib/z_calculator.rb
@@ -1,8 +1,19 @@
+require "pry"
+require "numbers_in_words"
+
 module ZCalculator
-  def one(*arg)
-    2
+  def respond_to_missing?
+    true
   end
 
-  def plus(number)
+  def method_missing(method, *args)
+    num = NumbersInWords.in_numbers(method.to_s)
+
+    return num unless args.any?
+    num.public_send(*args.first)
+  end
+
+  def plus(coeerced_number)
+    ["+", coeerced_number]
   end
 end
--- z-calculator/spec/z_calculator_spec.rb
+++ z-calculator/spec/z_calculator_spec.rb
@@ -2,7 +2,11 @@ require "z_calculator"
 
 RSpec.describe ZCalculator do
   include described_class
-  it "adds 2 number" do
+  it "adds one and one" do
     expect(one(plus(one))).to eq(2)
+  end
+
+  it "adds two and two" do
+    expect(two(plus(two))).to eq(4)
   end
 end
